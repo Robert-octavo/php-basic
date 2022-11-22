9cc1ca42035e459590cfb82e8be4e4ef1328effa
Robert Ortega <98441105+Robert-octavo@users.noreply.github.com>
2022-11-21 22:03:34 -0500

    Add config file

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+error_reporting( E_ALL );
+ini_set( 'display_error', 1 );
+
+define( 'SITE_URL', 'http://localhost/tu-proyecto' );
+define( 'SITE_LANG', [ 'es', 'spa', 'es_ES' ] );
+
+// conexion a Base de Datos
+
+define( 'DB_HOST', 'localhost' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', '' );
+define( 'DB_DATABASE', 'microcms' );
+define( 'DB_PORT', '3306' );
